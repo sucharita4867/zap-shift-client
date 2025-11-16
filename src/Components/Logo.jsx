6ef2744c8d2478f14c17c1396a5f57f1d3d889ef
sucharita4867 <sucharitasardar7@gmail.com>
2025-11-16 13:14:09 +0530

    added this rpoject stugter and root file

---
+++ src/Components/Logo.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import logo from '../assets/logo/png'
+const Logo = () => {
+      return (
+            <div>
+                  
+            </div>
+      );
+};
+
+export default Logo;
---
+++ src/Pages/Home/Home/Home.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Home = () => {
+  return <div>this id home</div>;
+};
+
+export default Home;
