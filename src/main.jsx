8fa8850bcc8e5da9a37cfbf2ab2eb235e35431bf
sucharita4867 <sucharitasardar7@gmail.com>
2025-11-17 12:07:24 +0530

    added all assets

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,10 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.jsx";
-import { router } from "./router/Router.jsx";
-import { RouterProvider } from "react-router";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
 
-createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>
-);
+    <App />
+  </StrictMode>,
+)
