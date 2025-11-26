f27e86ec8c06a4b74bc2c3548ee5cb19ba624c6e
sucharita4867 <sucharitasardar7@gmail.com>
2025-11-26 20:28:52 +0530

    create a new hook user apis

---
+++ src/Hooks/useRole.jsx
@@ -0,0 +1,19 @@
+import { useQuery } from "@tanstack/react-query";
+import React from "react";
+import useUserAuth from "./useUserAuth";
+import useAxiosSecure from "./useAxiosSecure";
+
+const useRole = () => {
+  const { user } = useUserAuth();
+  const axiosSecure = useAxiosSecure();
+  const { data: role = "user" } = useQuery({
+    queryKey: ["user-role", user.email],
+    queryFn: async () => {
+      const res = await axiosSecure.get(`/users/${user.email}/role`);
+      return res.data;
+    },
+  });
+  return <div></div>;
+};
+
+export default useRole;
--- src/Pages/Dashboard/UserManagement/UserManagement.jsx
+++ src/Pages/Dashboard/UserManagement/UserManagement.jsx
@@ -1,19 +1,113 @@
 import { useQuery } from "@tanstack/react-query";
 import React from "react";
 import useAxiosSecure from "../../../Hooks/useAxiosSecure";
+import { FaUserShield } from "react-icons/fa";
+import { FiShieldOff } from "react-icons/fi";
+import Swal from "sweetalert2";
 
 const UserManagement = () => {
   const axiosSecure = useAxiosSecure();
-  const { data: users = [] } = useQuery({
+  const { refetch, data: users = [] } = useQuery({
     queryKey: ["users"],
     queryFn: async () => {
       const res = await axiosSecure.get(`/users`);
       return res.data;
     },
   });
+
+  const handleMakeUser = (user) => {
+    const roleInfo = { role: "admin" };
+    axiosSecure.patch(`/users/${user._id}`, roleInfo).then((res) => {
+      console.log(res.data);
+      if (res.data.modifiedCount) {
+        refetch();
+        Swal.fire({
+          position: "top-end",
+          icon: "success",
+          title: `${user.displayName}marked ad Admin`,
+          showConfirmButton: false,
+          timer: 2000,
+        });
+      }
+    });
+  };
+
+  const handleRemoveAdmin = (user) => {
+    const roleInfo = { role: "user" };
+    axiosSecure.patch(`/users/${user._id}`, roleInfo).then((res) => {
+      if (res.data.modifiedCount) {
+        refetch();
+        Swal.fire({
+          position: "top-end",
+          icon: "success",
+          title: `${user.displayName}removed from  Admin`,
+          showConfirmButton: false,
+          timer: 2000,
+        });
+      }
+    });
+  };
   return (
     <div>
       <h2 className="text-secondary text-4xl">Manage Users : {users.length}</h2>
+      {/* tabel */}
+      <div className="overflow-x-auto">
+        <table className="table">
+          {/* head */}
+          <thead>
+            <tr>
+              <th>No</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Role</th>
+              <th>Admin Action</th>
+              <th>Others Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user, index) => (
+              <tr key={user._id}>
+                <th>{index + 1}</th>
+                <th>
+                  <div className="flex items-center gap-3">
+                    <div className="avatar">
+                      <div className="mask mask-squircle h-12 w-12">
+                        <img
+                          src={user.photoURL}
+                          alt="Avatar Tailwind CSS Component"
+                        />
+                      </div>
+                    </div>
+                    <div>
+                      <div className="font-bold">{user.displayName}</div>
+                    </div>
+                  </div>
+                </th>
+                <th>{user.email}</th>
+                <td>{user.role}</td>
+                <td>
+                  {user.role === "admin" ? (
+                    <button
+                      onClick={() => handleRemoveAdmin(user)}
+                      className="btn bg-red-500 "
+                    >
+                      <FiShieldOff />
+                    </button>
+                  ) : (
+                    <button
+                      onClick={() => handleMakeUser(user)}
+                      className="btn mr-2 bg-green-500"
+                    >
+                      <FaUserShield />
+                    </button>
+                  )}
+                </td>
+                <th>Action</th>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
