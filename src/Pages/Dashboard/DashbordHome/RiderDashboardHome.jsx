757595d78bf6c38f3b65275ffcc2970fe7c37342
sucharita4867 <sucharitasardar7@gmail.com>
2025-12-02 16:56:54 +0530

    added this task

--- src/Pages/Dashboard/DashbordHome/RiderDashboardHome.jsx
+++ src/Pages/Dashboard/DashbordHome/RiderDashboardHome.jsx
@@ -1,9 +1,22 @@
+import { useQuery } from "@tanstack/react-query";
 import React from "react";
+import useAxiosSecure from "../../../Hooks/useAxiosSecure";
 
 const RiderDashboardHome = () => {
+  const axiosSecure = useAxiosSecure();
+
+  const { data } = useQuery({
+    queryKey: ["delivery-status-stats"],
+    queryFn: async () => {
+      const res = await axiosSecure.get("/riders/delivery-par-day");
+      return res.data;
+    },
+  });
   return (
     <div>
       <h2 className="text-4xl text-secondary">RiderDashboardHome</h2>
+      <button className="btn bag-primary">out deliveris</button>
+      <h2 className="text-4xl text-secondary">{console.log(data)}</h2>
     </div>
   );
 };
