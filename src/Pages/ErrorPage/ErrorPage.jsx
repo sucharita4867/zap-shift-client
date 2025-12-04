eb67e99d2ec78660de4abb1dcb89b93212b4d1a1
sucharita4867 <sucharitasardar7@gmail.com>
2025-12-04 20:42:39 +0530

    errorpage is complete

--- src/Pages/ErrorPage/ErrorPage.jsx
+++ src/Pages/ErrorPage/ErrorPage.jsx
@@ -1,11 +1,31 @@
 import React from "react";
+import Lottie from "lottie-react";
 import ErrorAnimation from "../../assets/animations/error.json";
+import Navbar from "../Shred/Navbar";
+import Footer from "../Shred/Footer";
+import { Link } from "react-router";
 
 const ErrorPage = () => {
   return (
-    <div>
-      <h1>error page {ErrorAnimation}</h1>
-      <h1>{console.log(ErrorAnimation)}</h1>
+    <div className="w-11/12 mx-auto">
+      <div>
+        <Navbar />
+      </div>
+      <div className=" my-8 flex p-6 flex-col justify-center items-center bg-white rounded-xl">
+        <div className=" w-100">
+          <Lottie animationData={ErrorAnimation} loop={true} />
+        </div>
+        <h1 className="text-4xl font-bold my-4 text-secondary text-center">
+          Error 404
+        </h1>
+        <button className="btn  bg-primary">
+          <Link to="/">Go Home</Link>
+        </button>
+      </div>
+
+      <div>
+        <Footer />
+      </div>
     </div>
   );
 };
