044a54682480ecde39ece0b8d5cdf45de907a49c
sucharita4867 <sucharitasardar7@gmail.com>
2025-12-04 20:48:32 +0530

    create a loading sppner in json

--- src/Components/Loading/Loading.jsx
+++ src/Components/Loading/Loading.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import Lottie from "lottie-react";
+import loadingAnimation from "../../assets/animations/loading.json";
 
 const Loading = () => {
   return (
-    <div className="flex justify-center items-center text-2xl text-secondary">
-      <span className="loading loading-dots loading-xl"></span>
+    <div className="flex justify-center items-center min-h-screen">
+      <div className="w-46">
+        <Lottie animationData={loadingAnimation} loop={true} />
+      </div>
     </div>
   );
 };
